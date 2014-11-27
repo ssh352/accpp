8017a99ac33da42f86788cf8f2527bd4ac87e5a7
wuhao <wuhao@msu.edu>
2014-11-27 16:06:16 -0500

    finally, finished 5-1

--- chapter05/5-1.cpp
+++ chapter05/5-1.cpp
@@ -81,7 +81,7 @@ vector<string> rotate_lines(vector<string>& original_lines)
 	return new_vec;
 }
 
-std::vector<string> split_by_char(const string& s)
+std::vector<string> split_by_char(const string& s, const char& split_sign = '\\')
 {
 	typedef string::size_type string_size;
 	string_size i = 0;
@@ -98,7 +98,7 @@ std::vector<string> split_by_char(const string& s)
 		// find end of next word
 		string_size j = i;
 		// invariant: none of the characters in range `['original `j', current `j)' is a space
-		while (j != s.size() && s[j] != '\\')
+		while (j != s.size() && s[j] != split_sign)
 			++j;
 
 		// if we found some nonwhitespace characters
@@ -116,6 +116,7 @@ std::vector<string> split_by_char(const string& s)
 
 void output(vector<string>& f_vec)
 {
+	size_t col_cnt = 2;
 	vector<string>::iterator iter;
 	for(iter = f_vec.begin(); iter != f_vec.end(); ++iter)
 	{
@@ -123,14 +124,38 @@ void output(vector<string>& f_vec)
 		{
 			// replace back slash
 			vector<string> cols = split_by_char((*iter));
-			cout << std::setw(30) << cols[0];
-			cout << '\t' << cols[1] << endl;
+
+			// flip 2 strings
+			if (cols.size() == col_cnt)
+			{
+				cout << std::setw(30) << cols[1];
+				cout << '\t' << cols[0] << endl;
+			}
+			else
+			{
+				std::cerr << "not right size" << endl;
+			}
 		}
 		else
 		{
 			cout << (*iter) << endl;
 		}
 	}
+}
+
+// comparison, not case sensitive.
+// this function was copied from http://www.cplusplus.com/reference/list/list/sort/
+// much appreciated
+bool compare_nocase (const std::string& first, const std::string& second)
+{
+  unsigned int i=0;
+  while ( (i<first.length()) && (i<second.length()) )
+  {
+    if (tolower(first[i])<tolower(second[i])) return true;
+    else if (tolower(first[i])>tolower(second[i])) return false;
+    ++i;
+  }
+  return ( first.length() < second.length() );
 }
 
 int main()
@@ -146,8 +171,12 @@ int main()
 	// step 1:
 	shuffled_vec = rotate_lines(m_vec);
 
+	// for (size_t i = 0; i != shuffled_vec.size(); ++i)
+	// {
+	// 	cout << shuffled_vec[i] << endl;
+	// }
 	// step 2:
-	sort(shuffled_vec.begin(), shuffled_vec.end());
+	sort(shuffled_vec.begin(), shuffled_vec.end(), compare_nocase);
 
 	// step 3: output and split
 	output(shuffled_vec);
